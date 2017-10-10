e8f24ddd940cca63b0795061738811bd983791b2
golam chowdhury <adnan333bd@gmail.com>
2017-10-10 13:20:29 +0600

    Add the firebase project's config in the app

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS = {
+    apiKey: "AIzaSyBWhjNbnPUv1amnsLPQBOo7SdSA3IM9pBg",
+    authDomain: "fir-crud-c3009.firebaseapp.com",
+    databaseURL: "https://fir-crud-c3009.firebaseio.com",
+    projectId: "fir-crud-c3009",
+    storageBucket: "fir-crud-c3009.appspot.com",
+    messagingSenderId: "427869654171"
+  };
